1a085de0311da0dcc4b3d5a32449709a373623fa
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-10-21 19:13:31 +0200

    Complete

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
         <form action="{{route('posts.store')}}" method="post">
-        @csrf
-        @method('POST')
-        <div class="form-group">
-            <label for="title">Titolo</label>
-            <input type="text" name="title" placeholder="inserisci un titolo..">
-        </div>
-        <div class="form-group">
-            <label for="body">Body</label>
-            <textarea class="form-control" name="body" placeholder="inserisci un testo" rows="3"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+            @csrf
+            @method('POST')
+            <div class="form-group">
+                <label for="title">Titolo</label>
+                <input type="text" name="title" placeholder="inserisci un titolo..">
+            </div>
+            <div class="form-group">
+                <label for="body">Body</label>
+                <textarea class="form-control" name="body" placeholder="inserisci un testo" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
