79a61a7fed0b0c19bed2acbca77c23adad4ccb96
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-10-23 15:21:44 +0200

    Complete

--- resources/views/guest/post.blade.php
+++ resources/views/guest/post.blade.php
@@ -8,16 +8,6 @@
             <h5 class="card-title">{{$post->title}}</h5>
             <h5 class="card-title">{{$post->user->name}}</h5>
             <p class="card-text">{{$post->body}}</p>
-            @if ($post->user->name == Auth::user()->name)
-                <div class="d-flex">
-                    <button type="submit" class="btn btn-primary mr-3"><a href="{{route('posts.edit',$post->id)}}" class="card-link text-white">EDIT</a></button>
-                        <form action="{{route('posts.destroy',$post->id)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">DELETE</button>
-                    </form>
-                </div>            
-            @endif
             <div class="card-img mt-3" style="height:5rem;">
                 <img src="{{Storage::url($post->img)}}" class="img-fluid" alt="{{$post->title}}">
             </div>          
